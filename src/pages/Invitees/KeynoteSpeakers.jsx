32a4b9f4420838c238a08bbc0d5fd1cdba83da94
Prince Kumar Chaurasia <princechaurasiaofficial24@gmail.com>
2024-08-29 21:45:30 +0530

    keynote speaker details updated

--- src/pages/Invitees/KeynoteSpeakers.jsx
+++ src/pages/Invitees/KeynoteSpeakers.jsx
@@ -4,32 +4,131 @@ const KeynoteSpeakers = () => {
   return (
     <div className="md:px-16 px-5 my-3 md:min-h-screen">
       <div>
-        <p className="md:text-5xl text-2xl text-center font-serif font-bold">Proposed Keynote Speakers</p>
+        <p className="md:text-5xl text-2xl text-center font-serif font-bold">
+          Proposed Keynote Speakers
+        </p>
         <div className="h-1 mt-2 rounded-xl bg-gradient-to-r from-white via-black to-white relative" />
       </div>
       <div className="md:px-16 mt-5 text-lg text-justify tracking-wide space-y-3">
         <ul className="list-disc list-inside space-y-2">
-          <li><span className="highlight">Prof. Mike Hinchey, Professor, University of Limerick, Ireland & President, IFIP</span></li>
-          <li><span className="highlight">Prof. Vincenzo Piuri, Professor, University of Milan, Italy</span></li>
-          <li><span className="highlight">Mr. Aninda Bose, Senior Editor, Springer Nature, UK</span></li>
-          <li><span className="highlight">Prof. João Manuel RS Tavares, Faculdade de Engenharia da Universidade do Porto, Portugal</span></li>
-          <li><span className="highlight">Prof. Milan Tuba, Vice Rector, Singidunum University, Belgrade, Serbia</span></li>
-          <li><span className="highlight">Prof. Rosalina Babo, Professor, Polytechnic Institute of Porto, Portugal</span></li>
-          <li><span className="highlight">Prof. Zdzislaw Polkowski, Professor, Jan Wyzykowski University, Poland</span></li>
-          <li><span className="highlight">Prof. Vladan Devedzic, Professor of Computer Science, University of Belgrade, Serbia</span></li>
-          <li><span className="highlight">Prof. Bala Subramanian Raman, Professor, CSE, IIT Roorkee</span></li>
-          <li><span className="highlight">Prof. Ibrahim A. Hameed, Professor, ICT, Norwegian University of Science and Technology (NTNU), Ålesund, Norway</span></li>
-          <li><span className="highlight">Dr. Dharm Singh, Namibia University of Science and Technology, Namibia</span></li>
-          <li><span className="highlight">Dr. Yogesh Singh Chauhan, Professor, Dept. of ECE, IIT Kanpur</span></li>
-          <li><span className="highlight">Prof. Maya Ingle, DAVV, Indore</span></li>
-          <li><span className="highlight">Prof. R.S. Salaria, Professor, CSE, Guru Nanak Technical Institutions, Hyderabad</span></li>
-          <li><span className="highlight">Dr. Satish Kumar Singh, Professor, IT, IIIT Allahabad</span></li>
-          <li><span className="highlight">Prof. M. Hanumanthappa, Professor & Director, CS, Bangalore University, Bangalore</span></li>
-          <li><span className="highlight">Prof. Suresh Chandra Satapathy, Professor CSE, KIIT Deemed to be University, Bhubaneswar</span></li>
-          <li><span className="highlight">Prof. Durgesh Kumar Mishra, Professor, CSE, Sri Aurobindo Institute of Technology, Indore</span></li>
-          <li><span className="highlight">Prof. Vipin Tyagi, JUET, India & Secretary, Computer Society of India</span></li>
-          <li><span className="highlight">Prof. M. N. Hoda, Director, Bharti Vidyapeeth University, New Delhi</span></li>
-          <li><span className="highlight">Dr. Nilanjan Dey, Asso. Prof., CSE, Techno International New Town, Kolkata</span></li>
+          <li>
+            <span className="highlight">
+              Prof. Mike Hinchey, Professor, University of Limerick, Ireland &
+              President, IFIP
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Prof. Vincenzo Piuri, Professor, University of Milan, Italy
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Mr. Aninda Bose, Senior Editor, Springer Nature, UK
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Prof. João Manuel RS Tavares, Faculdade de Engenharia da
+              Universidade do Porto, Portugal
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Prof. Milan Tuba, Vice Rector, Singidunum University, Belgrade,
+              Serbia
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Prof. Rosalina Babo, Professor, Polytechnic Institute of Porto,
+              Portugal
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Prof. Zdzislaw Polkowski, Professor, Jan Wyzykowski University,
+              Poland
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Prof. Vladan Devedzic, Faculty of Organisational Sciences, Dept.
+              of Software Engineering, University of Belgrade, Serbia
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Prof. Bala Subramanian Raman, Professor, CSE, IIT Roorkee
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Prof. Ibrahim A. Hameed, Professor, ICT, Norwegian University of
+              Science and Technology (NTNU), Ålesund, Norway
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Dr. Dharm Singh, Namibia University of Science and Technology,
+              Namibia
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Dr. Yogesh Singh Chauhan, Professor, Dept. of ECE, IIT Kanpur
+            </span>
+          </li>
+          <li>
+            <span className="highlight">Prof. Maya Ingle, DAVV, Indore</span>
+          </li>
+          <li>
+            <span className="highlight">
+              Prof. R.S. Salaria, Professor, CSE, Guru Nanak Technical
+              Institutions, Hyderabad
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Dr. Satish Kumar Singh, Professor, IT, IIIT Allahabad
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Prof. M. Hanumanthappa, Professor & Director, CS, Bangalore
+              University, Bangalore
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Prof. Suresh Chandra Satapathy, Professor CSE, KIIT Deemed to be
+              University, Bhubaneswar
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Prof. Durgesh Kumar Mishra, Professor, CSE, Sri Aurobindo
+              Institute of Technology, Indore
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Prof. Vipin Tyagi, JUET, India & Secretary, Computer Society of
+              India
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Prof. M. N. Hoda, Director, Bharti Vidyapeeth University, New
+              Delhi
+            </span>
+          </li>
+          <li>
+            <span className="highlight">
+              Dr. Nilanjan Dey, Asso. Prof., CSE, Techno International New Town,
+              Kolkata
+            </span>
+          </li>
         </ul>
       </div>
     </div>
